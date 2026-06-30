c74731c2a86385bc07ede6c90e60d3bee29ac9e4
api <gkaur71591@gmail.com>
2026-06-30 17:30:40 -0400

    fix env variable handling for render deployment

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    SECRET_KEY=os.getenv("SECRET_KEY")
-    ALGORITHM=os.getenv("ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret")
+    ALGORITHM = os.getenv("ALGORITHM", "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES = int(
+        os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
+    )
 
-setting=Settings()    
+setting = Settings()
